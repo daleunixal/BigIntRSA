6a5d143463a3a2638f8f0286c6122004f1dc572e
Nikita Zerniukov <Nikita.Zerniukov@urfu.me>
2021-03-11 12:26:32 +0500

    Initial commit

---
+++ BigIntRSA/BigInt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigIntRSA
+{
+    public class BigInt
+    {
+        private static char[] sign = {'+', '-'};
+        
+        private readonly List<int> _store;
+        private readonly bool _isNegative = false; // If false = '+' else '-'
+
+
+        public BigInt()
+        {
+            _store = new List<int>() {0};
+        }
+        
+        public BigInt(string number)
+        {
+            if (char.IsDigit(number[0]))
+            {
+                _store = ParseString(number);
+                return;
+            }
+
+            if (number[0] == '+' || number[0] == '-')
+            {
+                if (number[0] == '-')
+                {
+                    _isNegative = true;
+                }
+                _store = ParseString(number.Trim(sign));
+            }
+            else
+            {
+                throw new ArgumentException($"Uncorrect input: {number}");
+            }
+        }
+
+        public BigInt(BigInt ds, bool? sign)
+        {
+            _store = ds._store;
+            _isNegative = sign ?? ds._isNegative;
+        }
+
+        public BigInt SwapSign()
+        {
+            var signToSwap = !_isNegative;
+            return new BigInt(this, signToSwap);
+        }
+
+        private List<int> ParseString(string number)
+        {
+            return number.ToCharArray().Select(x => Int32.Parse(x.ToString())).ToList();
+        }
+
+        public override string ToString()
+        {
+            return _isNegative ? '-' + string.Join("", _store) : String.Join("", _store);
+        }
+
+        public static BigInt operator %(BigInt firstInt, BigInt secondInt)
+        {
+            return a - b * a / b;
+        }
+
+        public static BigInt operator -(BigInt firstInt, BigInt secondInt)
+        { 
+            return firstInt + secondInt.SwapSign();
+        }
+
+        public static BigInt operator +(BigInt firstInt, BigInt secondInt)
+        {
+            
+        }
+    }
+}
---
+++ BigIntRSA/BigIntTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace BigIntRSA
+{
+    [TestFixture]
+    public class BigIntTests
+    {
+        private BigInt _myBigInt;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _myBigInt = new BigInt();
+        }
+
+        [Test]
+        public void ZeroInt()
+        {
+            Assert.AreEqual("0", _myBigInt.ToString());
+        }
+
+        [Test]
+        public void NegativeString()
+        {
+            var q = new BigInt("-123");
+            Assert.AreEqual("-123", q.ToString());
+        }
+
+        [Test]
+        public void PositiveString()
+        {
+            var q = new BigInt("3342");
+            Assert.AreEqual("3342", q.ToString());
+        }
+
+        [Test]
+        public void UncorrectStringThrows()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var q = new BigInt("fas231");
+            });
+        }
+
+        [Test]
+        public void Subtract()
+        {
+            var q = new BigInt("123");
+            var w = new BigInt("143");
+            Assert.AreEqual("31", w-q);
+        }
+    }
+}
